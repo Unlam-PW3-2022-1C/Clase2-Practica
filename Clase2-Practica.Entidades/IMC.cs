65d3b38308fe04ef609b4203ca35837477521568
Cris <c.fernandez.melian@hotmail.com>
2022-04-12 17:24:02 -0300

    API IMC Fernandez Melian

---
+++ Clase2-Practica.Entidades/IMC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Clase2_Practica.Entidades
+{
+    public class IMC
+    {
+        public DateTime Date { get; set; }
+
+        public string Nombre { get; set; } //por que aparece "string" en el json?
+
+        public double Peso { get; set; }
+
+        public double Altura { get; set; } //que es esto?
+
+        public double Imc { get; set; } // como evitar modificarlo en la pantalla?
+
+    }
+}
---
+++ Clase2-Practica.Servicios/IIMCService.cs
@@ -0,0 +1,13 @@
+﻿using Clase2_Practica.Entidades;
+using System;
+using System.Collections.Generic;
+
+namespace Clase2_Practica.Servicios
+{
+    public interface IIMCService
+    {
+        public IEnumerable<IMC> ObtenerTodos();
+        public IEnumerable<IMC> Insertar(IMC datos);
+        public IEnumerable<IMC> Borrar(string nombre);//se borra la informacion con poner el nombre
+    }
+}
