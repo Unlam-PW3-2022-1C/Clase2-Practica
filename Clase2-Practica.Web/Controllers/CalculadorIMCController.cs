6de24982e6afbb536c93e34e6c0f89fae7f4cdbf
NahuelGiorgi <nahuel.matias.giorgi@outlook.es>
2022-04-13 19:49:25 -0300

    Calculadora IMC (#5)
    pendiente agregar array con los mensajes

---
+++ Clase2-Practica.Web/Controllers/CalculadorIMCController.cs
@@ -0,0 +1,24 @@
+﻿using Clase2_Practica.Servicios.Implementaciones;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase2_Practica.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculadorIMCController : ControllerBase
+    {
+        readonly ICalculadorIMCService _calculadorIMCService;
+
+        public CalculadorIMCController(ICalculadorIMCService calculadorIMCService)
+        {
+            _calculadorIMCService = calculadorIMCService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public double GetIMC(float peso, float altura)
+        {
+            return _calculadorIMCService.CalcularIMC(peso, altura);
+        }
+    }
+}
